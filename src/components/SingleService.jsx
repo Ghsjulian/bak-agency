f93acf7d2deb825da8dd48cb2db2c161b2368fdb
Ghsjulian <ghsjulian@gmail.com>
2024-06-07 10:20:42 +0600

    Added File

--- src/components/SingleService.jsx
+++ src/components/SingleService.jsx
@@ -6,7 +6,43 @@ const SingleService = ({ data }) => {
     const [siteData, setSiteData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
+    const arr = {
+        web_development: {
+            service_img:
+                "http://localhost:8080/server/assets/images/agent_3.png",
+            service_heading: "Web Development & Design",
+            service_description:
+                "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
+        },
+        graphics_desgin: {
+            service_img:
+                "http://localhost:8080/server/assets/images/agent_3.png",
+            service_heading: "Web Development & Design",
+            service_description:
+                "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
+        },
+        seo_analysis: {
+            service_img:
+                "http://localhost:8080/server/assets/images/agent_3.png",
+            service_heading: "Web Development & Design",
+            service_description:
+                "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
+        },
+        video_editing: {
+            service_img:
+                "http://localhost:8080/server/assets/images/agent_3.png",
+            service_heading: "Web Development & Design",
+            service_description:
+                "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
+        },
+        content_writing: {
+            service_img:
+                "http://localhost:8080/server/assets/images/agent_3.png",
+            service_heading: "Web Development & Design",
+            service_description:
+                "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
+        }
+    };
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
@@ -36,29 +72,16 @@ const SingleService = ({ data }) => {
     if (siteData != null) {
         return (
             <>
-            
-            
-              {
-                siteData.map((data, index) => {
-                    return <h1> {data.service_heading} </h1>;
-                })
-              }
-                
-                
-                
-                
-                
-                
-                
-                
                 {/*
+                {arr.map((item, index) => {
+                    return <h1> {item.service_heading} </h1>;
+                })}
+*/}
+
                 <div data-aos="zoom-in" id="page" className="section">
                     <div className="service-layouts">
                         <img src={data.development} />
-                        <h2>
-                            Web Development & Design{" "}
-                            {siteData.web_development.service_heading}
-                        </h2>
+                        <h2>Web Development & Design</h2>
                         <p className="service-description">
                             We serve the best design and professional website,
                             our employees are so good they build professional
@@ -73,7 +96,6 @@ const SingleService = ({ data }) => {
                         </p>
                     </div>
                 </div>
-                */}
             </>
         );
     }
