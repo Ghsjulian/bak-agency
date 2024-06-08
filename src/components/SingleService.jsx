f2422b60e1d2b9f0f5a8ec992ebe25216b6f53ba
Ghsjulian <ghsjulian@gmail.com>
2024-06-08 12:35:13 +0600

    fixed

--- src/components/SingleService.jsx
+++ src/components/SingleService.jsx
@@ -19,7 +19,7 @@ const SingleService = ({ data }) => {
             type: "graphics-desgin",
             service_img:
                 "http://localhost:8080/server/assets/images/agent_3.png",
-            service_heading: "Web Development & Design",
+            service_heading: "Graphics Desgin And Logo",
             service_description:
                 "   We serve the best design and professional website, our employees are so good they build professional and user-friendly website . supposed you have planned for making a good qualities website , such as e commerce, template building and many more . Kindly please contact us. Explore our main service and offer we provide the best service all over the world , if you want to get ourservice please contact us right now we'll response you as soon as possible."
         },
@@ -75,29 +75,21 @@ const SingleService = ({ data }) => {
         return; // or an error message, etc.
     }
     if (siteData != null) {
-        return (
-            <>
-                {arr.map((item, index) => {
-                    if (data.type) {
-                        return (
-                            <div
-                                data-aos="zoom-in"
-                                id="page"
-                                className="section"
-                            >
-                                <div className="service-layouts">
-                                    <img src={item.service_img} />
-                                    <h2>{item.service_heading}</h2>
-                                    <p className="service-description">
-                                        {item.service_description}{" "}
-                                    </p>
-                                </div>
-                            </div>
-                        );
-                    }
-                })}
-            </>
-        );
+        console.log([siteData]);
+        const matchingItem = arr.find(item => item.type);
+        if (data.type.name === matchingItem.type) {
+            return (
+                <div data-aos="zoom-in" id="page" className="section">
+                    <div className="service-layouts">
+                        <img src={matchingItem.service_img} />
+                        <h2>{matchingItem.service_heading}</h2>
+                        <p className="service-description">
+                            {matchingItem.service_description}
+                        </p>
+                    </div>
+                </div>
+            );
+        }
     }
 };
 
