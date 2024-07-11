9208c72fa6fc761a46df7c5125de6fead52ccd08
Ghsjulian <ghsjulian@gmail.com>
2024-07-11 10:18:57 +0600

    Working On View Blogs

--- src/components/Viewblogs.jsx
+++ src/components/Viewblogs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import team_2 from "../assets/images/team_2.png";
+import { NavLink } from "react-router-dom";
+
 const Viewblogs = () => {
     return (
         <div data-aos="zoom-in" id="page" className="section">
@@ -7,23 +8,114 @@ const Viewblogs = () => {
             <br />
             <h2>Create Attractive Website Using Custom Coding</h2>
             <div className="blog-img">
-                <img src="/images/demo6.jpg" alt="Blog" />
+                <img src="/images/Website-Development.jpg" alt="Blog" />
             </div>
             <div className="content">
-                <div className="post-section">
-                <article className="text">
-                Stay ahead of the curve with the latest insights and trends in
-                digital marketing and online services. Our blog is your go-to
-                resource for expert advice, industry news, and thought
-                leadership articles. From the benefits of outsourcing to a
-                digital agency to the latest digital marketing techniques, our
-                blog covers it all.
-            </article>
+                <div className="left">
+                    <article>
+                        Meet our team expertises. We have a group of team and we
+                        can handle any task byu sharing our experience. Here is
+                        our team members name and their skills and experienc.
+                        Meet our team expertises. We have a group of team and we
+                        can handle any task byu sharing our experience. Here is
+                        our team members name and their skills and
+                        experienc.Meet our team expertises. We have a group of
+                        team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.Meet our team expertises. We have a
+                        group of team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.Meet our team expertises. We have a
+                        group of team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.Meet our team expertises. We have a
+                        group of team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.Meet our team expertises. We have a
+                        group of team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.Meet our team expertises. We have a
+                        group of team and we can handle any task byu sharing our
+                        experience. Here is our team members name and their
+                        skills and experienc.
+                    </article>
                 </div>
-                <div className="blog-section">
-                    <div className="blog-card"></div>
-                    <div className="blog-card"></div>
-                    <div className="blog-card"></div>
+                <div className="right">
+                    <h2>Related Blogs</h2>
+                    <div className="list">
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                        <NavLink to="/">
+                            <img
+                                src="/images/Website-Development.jpg"
+                                alt="blog"
+                            />
+                            <strong>
+                                Create Attractive E Commerce Website Using Custom design and Coding 
+                            </strong>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
