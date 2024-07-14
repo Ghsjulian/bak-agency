4607e2cbfa9d54b1be51e56c51e33e5aa4eb899e
Ghsjulian <ghsjulian@gmail.com>
2024-07-14 15:32:12 +0600

    bottom service sections styling

--- src/components/SingleService.jsx
+++ src/components/SingleService.jsx
@@ -131,27 +131,50 @@ const SingleService = ({ data }) => {
             </li>
           </div>
         </div>
-       </div>
-        <div className="service-desc">
-          <div className="service-col">
-            <h5>Search Engine Optimization</h5>
-            <p>
-
-               has over 12 years of experience in this particular section.
-              We offer professional and affordable SEO services<em> </em>to get
-              more customers from all Search Engines. DMS provides advanced
-              Local SEO services, Global Search Engine Optimization services,
-              <em> </em>On-page and off-page detail audit reports, profitable
-              keyword research, optimized content writing, on-page strategies
-              implementation, and off-page or link-building services.
-            </p>
-          </div>
-          <div className="service-col">
-            <img src="/images/seo_service.png" />
-          </div>
+      </div>
+      <div className="service-desc">
+        <div className="service-col">
+          <h4>Search Engine Optimization</h4>
+          <p>
+            Skyrocket Your Organic Traffic with Our SEO Expertise Get your
+            website seen by the right audience. Our data-driven SEO services
+            optimize your site for search engines, attracting qualified leads
+            and boosting organic traffic. We'll help you dominate search results
+            for relevant keywords, turning clicks into conversions.
+          </p>
+          <h4
+            style={{
+              textAlign: "center",
+              fontSize: "1.7rem",
+              marginTop : "4rem",
+              margin: ".5rem auto",
+            }}
+          >
+            Here's what sets our SEO services apart:
+          </h4>
         </div>
-
-        {/* <div className="service-layouts">
+        <div className="service-col">
+          <img src="/images/seo.png" />
+        </div>
+      </div>
+      <div className="next-service">
+      <p style={{
+            width : "100%",
+            maxWidth : "100%",
+            
+          }}>
+            <b>Targeted Keyword Strategy:</b> We identify the perfect keywords to
+            target, ensuring your website reaches the right audience. 
+            <br/>
+           <b> Technical SEO Optimization: </b> We ensure your website's foundation is
+            search-engine friendly, improving crawlability and user experience.<br/>
+            <b>Content Marketing Powerhouse:</b> We craft high-quality, SEO-optimized
+            content that engages visitors and drives conversions. <br/>
+            <b>Regular Reporting & Transparency: </b>Stay informed with clear reports that
+            track your progress and SEO success.
+          </p>
+      </div>
+      {/* <div className="service-layouts">
                 {!foundObject && <h2>Loading...</h2>}
                 <br />
                 <br />
@@ -171,7 +194,6 @@ const SingleService = ({ data }) => {
                 )}
             </div>
              */}
-     
     </div>
   );
 };
